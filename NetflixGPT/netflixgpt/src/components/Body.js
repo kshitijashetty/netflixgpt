1b3b35b6c520ea1c5333c5d10e83c0a4e2dc25de
kshitijashetty <kshitijashetty100@gmail.com>
2024-10-20 12:58:16 +0530

    commit 2

--- NetflixGPT/netflixgpt/src/components/Body.js
+++ NetflixGPT/netflixgpt/src/components/Body.js
@@ -4,13 +4,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Body = () => {
   const appRouter = createBrowserRouter([
-    { path: "/", element: <Login/> },
-    { path: "/browse", element: <Browse/> },
+    { path: "/", element: <Login /> },
+    { path: "/browse", element: <Browse /> },
   ]);
-  return (
-    <RouterProvider router={appRouter}>
-    </RouterProvider>
-  );
+  return <RouterProvider router={appRouter}></RouterProvider>;
 };
 
 export default Body;
